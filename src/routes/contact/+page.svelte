684f428f9c9a1fd24a6ebaf00578d1e927fb1405
BeanyZoldyck <86325894+BeanyZoldyck@users.noreply.github.com>
2026-03-03 20:09:56 -0500

    contacts

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -24,35 +24,38 @@
 
 		<div class="border-l-2 border-purple-700 pl-6">
 			<p class="mb-4 text-lg text-purple-400">Find me online:</p>
-			<div class="space-y-2">
-				<a
-					href="https://github.com/BeanyZoldyck"
-					target="_blank"
-					class="block text-purple-300 hover:text-purple-200"
-					title="github"
-				>
-					<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="1.5"
-						shape-rendering="geometricPrecision"
-						viewBox="0 0 24 24"
-						height="60"
-						width="60"
-						style="color: currentcolor;"
-						><path
-							d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
-						></path></svg
-					></a
-				>
+			<div class="space-y-4">
+				<div class="group relative">
+					<a
+						href="https://github.com/BeanyZoldyck"
+						target="_blank"
+						class=" text-purple-300 hover:text-purple-200"
+						title="github"
+					>
+						github
+						<svg
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							shape-rendering="geometricPrecision"
+							viewBox="0 0 24 24"
+							height="60"
+							width="60"
+							style="color: currentcolor;"
+							><path
+								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
+							></path></svg
+						></a
+					>
+				</div>
 				<a
 					href="https://linkedin.com/in/chukaez"
 					target="_blank"
 					class="block text-purple-300 hover:text-purple-200"
 					title="linkedin"
-					><svg
+					>linkedin<svg
 						fill="none"
 						stroke="currentColor"
 						stroke-linecap="round"
@@ -73,8 +76,7 @@
 					target="_blank"
 					class="block text-purple-300 hover:text-purple-200"
 					aria-label="MAL"
-				>
-					<svg
+					>myanimelist<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="70px"
 						height="70px"
