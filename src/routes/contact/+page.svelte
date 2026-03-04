dc30cb6bca71742e81518ffc065ea1b8930a01d9
beanyzoldyck <chukaezeoke79@gmail.com>
2026-03-04 10:12:04 -0500

    contact component

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,65 @@
+<script>
+import Contact from './contact.svelte';
+  const contacts = [{
+    name:'github',
+    link:"https://github.com/BeanyZoldyck",
+    svg: `<svg
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							shape-rendering="geometricPrecision"
+							viewBox="0 0 24 24"
+							height="60"
+							width="60"
+							style="color: currentcolor;"
+							><path
+								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
+							></path></svg
+						>`
+  },
+  {
+    name:'linkedin',
+    link:"https://linkedin.com/in/chukaez",
+    svg:`<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="1.5"
+						shape-rendering="geometricPrecision"
+						viewBox="0 0 24 24"
+						height="60"
+						width="60"
+						style="color: currentcolor;"
+						><path
+							d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+						></path><circle cx="4" cy="4" r="2"></circle></svg
+					>`
+  },
+  {
+    name:"myanimelist",
+    link:"https://myanimelist.net/profile/BeanyZoldyck",
+    svg:`<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="70px"
+						height="70px"
+						viewBox="0 0 48 48"
+						stroke="currentColor"
+						><path
+							class="a"
+							d="M40.5,42.5H7.5a2,2,0,0,1-2-2V7.5a2,2,0,0,1,2-2h33a2,2,0,0,1,2,2v33A2,2,0,0,1,40.5,42.5Z"
+						/><polyline class="a" points="32.04 19.3 32.04 28.7 36.74 28.7" /><polyline
+							class="a"
+							points="11.26 28.69 11.26 19.3 15.96 28.7 20.66 19.32 20.66 28.7"
+						/><line class="a" x1="28.4" y1="25.55" x2="24.3" y2="25.55" /><polyline
+							class="a"
+							points="23.24 28.7 26.35 19.3 29.46 28.7"
+						/></svg
+					>`
+  }]
+</script>
 <section class="py-20">
 	<h1 class="mb-8 text-4xl font-bold text-purple-300">contact</h1>
 
@@ -25,75 +87,10 @@
 		<div class="border-l-2 border-purple-700 pl-6">
 			<p class="mb-4 text-lg text-purple-400">find me online:</p>
 			<div class="space-y-4">
-				<div class="group relative">
-					<a
-						href="https://github.com/BeanyZoldyck"
-						target="_blank"
-						class=" text-purple-300 hover:text-purple-200"
-						title="github"
-					>
-						github
-						<svg
-							fill="none"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="1.5"
-							shape-rendering="geometricPrecision"
-							viewBox="0 0 24 24"
-							height="60"
-							width="60"
-							style="color: currentcolor;"
-							><path
-								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
-							></path></svg
-						></a
-					>
-				</div>
-				<a
-					href="https://linkedin.com/in/chukaez"
-					target="_blank"
-					class="block text-purple-300 hover:text-purple-200"
-					title="linkedin"
-					>linkedin<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="1.5"
-						shape-rendering="geometricPrecision"
-						viewBox="0 0 24 24"
-						height="60"
-						width="60"
-						style="color: currentcolor;"
-						><path
-							d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-						></path><circle cx="4" cy="4" r="2"></circle></svg
-					>
-				</a>
-				<a
-					href="https://myanimelist.net/profile/BeanyZoldyck"
-					target="_blank"
-					class="block text-purple-300 hover:text-purple-200"
-					aria-label="MAL"
-					>myanimelist<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="70px"
-						height="70px"
-						viewBox="0 0 48 48"
-						stroke="currentColor"
-						><path
-							class="a"
-							d="M40.5,42.5H7.5a2,2,0,0,1-2-2V7.5a2,2,0,0,1,2-2h33a2,2,0,0,1,2,2v33A2,2,0,0,1,40.5,42.5Z"
-						/><polyline class="a" points="32.04 19.3 32.04 28.7 36.74 28.7" /><polyline
-							class="a"
-							points="11.26 28.69 11.26 19.3 15.96 28.7 20.66 19.32 20.66 28.7"
-						/><line class="a" x1="28.4" y1="25.55" x2="24.3" y2="25.55" /><polyline
-							class="a"
-							points="23.24 28.7 26.35 19.3 29.46 28.7"
-						/></svg
-					>
-				</a>
+        {#each contacts.sort(()=>Math.random()-0.5) as contact}
+           <!-- content here -->
+          <Contact name={contact.name} link={contact.link} svg={contact.svg}></Contact>
+        {/each}
 			</div>
 		</div>
 	</div>
---
+++ src/routes/contact/contact.svelte
@@ -0,0 +1,18 @@
+
+<script>
+  export let link;
+  export let name;
+  export let svg;
+</script>
+				<div class="group relative">
+					<a
+						href={link}
+						target="_blank"
+						class=" text-purple-300 hover:text-purple-200"
+						title="github"
+					>
+    {name}
+								{@html svg}
+						</a
+					>
+				</div>
