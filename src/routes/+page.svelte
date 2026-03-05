53ece4b3b89553af40b900db6477e6c518104218
BeanyZoldyck <86325894+BeanyZoldyck@users.noreply.github.com>
2026-03-05 08:33:24 -0500

    funny lowercase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,12 @@
 	<h1 class="mb-6 text-5xl font-bold text-purple-300 md:text-6xl">
 		hi, i'm <span class="text-purple-200">chuka</span>
 	</h1>
-	<p class="mb-8 text-xl text-purple-400 md:text-2xl">i do things sometimes</p>
+	<div class="inline-flex">
+		<p class="mb-8 text-xl text-purple-400 md:text-2xl">i do things sometimes</p>
+		<p class="mb-8 text-xl text-black hover:text-purple-400 md:text-2xl">
+			, typically in lowercase
+		</p>
+	</div>
 	<div class="flex gap-4">
 		{#each things.sort(() => Math.random() - 0.5) as thing}
 			<Link {thing}></Link>
