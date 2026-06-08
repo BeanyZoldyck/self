aacee2c053aa7b0f0e1062bb1e1478e349c02e57
BeanyZoldyck <beanyzoldyck@gmail.com>
2026-06-08 00:34:54 -0400

    email page

---
+++ src/routes/email/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<section class="py-20">
+	<h1 class="mb-8 text-4xl font-bold text-purple-300">email</h1>
+
+	<div class="space-y-6">
+		<div class="border-l-2 border-purple-700 pl-6">
+			<p class="text-lg text-purple-400">sorry for the longwinded nature, I'm irrationally scared of bots</p>
+		</div>
+
+		<div class="border-l-2 border-purple-700 pl-6">
+			<p class="text-lg text-purple-200">My email is the first letter of my first name, my last name, at umich.edu :)</p>
+		</div>
+</section>
