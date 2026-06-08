dc3298180565c2289ad4d87187beca20356ea609
BeanyZoldyck <beanyzoldyck@gmail.com>
2026-06-07 23:48:25 -0400

    newline script
    there is no feasible measure by which I call myself a serious developer

---
+++ newline.py
@@ -0,0 +1,17 @@
+import sys
+args = sys.argv
+if len(args) > 1:
+    file = args[1]
+else:
+    file = input("Path to file: ")
+
+
+with open(file) as f:
+    content = f.read()
+
+content_parsed = content.replace("\\n\n","\\n")
+content_final = content_parsed.replace("\\n","\n")
+
+with open(file.replace('.','_test.'), 'w') as g:
+    g.write(content_final)
+
