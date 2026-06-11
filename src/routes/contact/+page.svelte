52822213d9963447dcc3fd728b10763aeabc5b84
BeanyZoldyck <beanyzoldyck@gmail.com>
2026-06-11 10:42:14 -0400

    email link

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -42,7 +42,7 @@
 		{
 			name: 'twitter',
 			link: 'https://x.com/chuka_ze',
-			svg: `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="60" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
 </svg>`
 		},
@@ -63,7 +63,7 @@
 			<p class="mb-2 text-lg text-purple-400">feel free to reach out:</p>
 			<Contact
 				contact={{
-					link: 'https://beanyzoldyck.github.io/self/email',//wait I'm hilarious
+					link: 'https://beanyzoldyck.github.io/email',//wait I'm hilarious
 					name: 'email',
 					svg: `<svg
 					fill="none"
